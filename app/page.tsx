68f494bb414072337a024e0068348fdd6e67ee38
Bruno S. Duarte <brunosduarte@hotmail.com>
2025-08-20 17:10:11 -0300

    chore: initial page implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,73 @@
 "use client"
 
+import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
+      {/* header */}
       <Header />
-      <h1>Welcome to the Barber Shop</h1>
-      <Button variant="default">Book Now</Button>
+      <div className="p-5">
+        {/* TEXTO */}
+        <h2 className="text-xl font-bold"> Olá, Bruno!</h2>
+        <p>Quarta-feira, 20 de agosto.</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com BsD Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* AGENDAMENTOS */}
+        <h2 className="mt-6 mb-3 text-sm font-bold text-gray-400 uppercase">
+          Agendamentos
+        </h2>
+
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia BsD</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
+
+export default Home
