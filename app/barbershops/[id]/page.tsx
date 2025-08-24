d27309a35fc38f8e1df5aad0e153168a4947e3cd
Bruno S. Duarte <brunosduarte@hotmail.com>
2025-08-24 19:58:19 -0300

    feat: add BarbershopPage component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { db } from "@/app/_lib/prisma"
+
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  const barbershop = await db.barberShop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+
+  return (
+    <>
+      <h1>{barbershop?.name}</h1>
+    </>
+  )
+}
+
+export default BarbershopPage
