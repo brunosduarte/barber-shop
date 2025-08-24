5f2a647adc92f1faef50417edb96098327d3a732
Bruno S. Duarte <brunosduarte@hotmail.com>
2025-08-24 18:17:36 -0300

    feat: add BookingITem component

--- app/_components/quick-search.tsx
+++ app/_components/quick-search.tsx
@@ -1,37 +1,7 @@
+import { quickSearchOptions } from "../_constants/search"
 import { Button } from "./ui/button"
 import Image from "next/image"
 
-interface QuickSearchOption {
-  imageUrl: string
-  title: string
-}
-
-const quickSearchOptions: QuickSearchOption[] = [
-  {
-    imageUrl: "/cabelo.svg",
-    title: "Cabelo",
-  },
-  {
-    imageUrl: "/barba.svg",
-    title: "Barba",
-  },
-  {
-    imageUrl: "/acabamento.svg",
-    title: "Acabamento",
-  },
-  {
-    imageUrl: "/massagem.svg",
-    title: "Massagem",
-  },
-  {
-    imageUrl: "/sobrancelha.svg",
-    title: "Sobrancelha",
-  },
-  {
-    imageUrl: "/hidratacao.svg",
-    title: "Hidratação",
-  },
-]
 const QuickSearch = () => {
   return (
     <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
