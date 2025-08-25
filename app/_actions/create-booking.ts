995aed516f3a0311058ea7b0554b6258cc15e203
Bruno S. Duarte <brunosduarte@hotmail.com>
2025-08-25 09:46:27 -0300

    feat: create, delete and get bookings, BookingsSummary and auth connections

---
+++ app/_actions/create-booking.ts
@@ -0,0 +1,37 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
+
+interface CreateBookingParams {
+  serviceId: string
+  date: Date
+}
+
+export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
+
+  const service = await db.barbershopService.findUnique({
+    where: { id: params.serviceId },
+    select: { barbershopId: true },
+  })
+
+  if (!service) {
+    throw new Error("Serviço não encontrado")
+  }
+
+  await db.booking.create({
+    data: {
+      ...params,
+      userId: (user.user as { id: string }).id,
+      barbershopId: service.barbershopId,
+    },
+  })
+  revalidatePath("/barbershops/[id]")
+  revalidatePath("/bookings")
+}
