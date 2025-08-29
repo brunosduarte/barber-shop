a577be5e117974d51930a99c1fa02e30ff4ddfab
Bruno S. Duarte <brunosduarte@hotmail.com>
2025-08-29 09:46:13 -0300

    fix: fixed issue of generated prisma dependency

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,22 +1,18 @@
-import { PrismaClient } from "../../generated/prisma"
-import { PrismaClient as StandardPrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 import { withAccelerate } from "@prisma/extension-accelerate"
 
 const prismaClient = new PrismaClient()
 const prisma = prismaClient.$extends(withAccelerate())
-const standardPrismaClient = new StandardPrismaClient()
 
 const globalForPrisma = global as unknown as {
   prisma: typeof prisma
   prismaClient: typeof prismaClient
-  standardPrismaClient: typeof standardPrismaClient
 }
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
   globalForPrisma.prismaClient = prismaClient
-  globalForPrisma.standardPrismaClient = standardPrismaClient
 }
 
 export const db = prisma
-export const prismaClientForAdapter = standardPrismaClient
+export const prismaClientForAdapter = prismaClient
